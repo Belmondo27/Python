687b6a6f83ba491cc7a5386dee8ca85c69a0eb30
Belmondo27 <zekan27@gmail.com>
2022-12-12 20:44:32 +0100

    Lekcja ze stringami

--- Lekcja1.py
+++ Lekcja1.py
@@ -1,3 +1,12 @@
 print("Hello World")
 greeting = "Hello World"
 print(greeting)
+text1 = 'TEST'
+text2 = "TEST / TEST"
+text3 = "Karta graficzna SAPPHIRE Radeon RX 6700 Gaming OC 10GBKarta graficzna SAPPHIRE Radeon \
+         RX 6700 Gaming OC 10GBKarta graficzna SAPPHIRE Radeon RX 6700 Gaming OC 10GBKarta graficzna \ SAPPHIRE Radeon RX 6700 Gaming OC 10GBKarta graficzna SAPPHIRE Radeon RX 6700 Gaming OC \
+                    10GBKarta graficzna SAPPHIRE Radeon RX 6700 Gaming OC 10GBKarta graficzna SAPPHIRE \
+                            Radeon RX 6700 Gaming OC 10GBKarta graficzna SAPPHIRE Radeon RX 6700 Gaming OC 10GB"
+print(text1)
+print(text2)
+print(text3)
---
+++ Lekcja2.py
@@ -0,0 +1,7 @@
+first_name= "Damian"
+last_name = "Zakryś"
+full_name = first_name + " " + last_name
+print(full_name)
+print(len(full_name))
+print(first_name[2])
+print(last_name[-1])
