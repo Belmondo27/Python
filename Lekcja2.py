b0d8907827525abe5703e52ef30f51f0d3975af6
Belmondo27 <zekan27@gmail.com>
2022-12-16 20:14:42 +0100

    Koniec rodziału 4.3

--- Lekcja2.py
+++ Lekcja2.py
@@ -6,6 +6,46 @@ print(len(full_name))
 print(first_name[2])
 print(last_name[-1])
 print(first_name[0:6])
-word = "zing"
+word = "ZINGBA"
 word = "ba" + word[:4] + "a"
-print(word)
+word.lower()
+print(word.lower())
+test="CHOMIK"
+print(test.lower())
+print(test.lstrip().lower())
+print(test.startswith("CH"))
+print(test.endswith("MIK"))
+test=test.lower()
+print(test.startswith("CH"))
+##############################################################
+
+var1 = "Animals"
+var2 = "Badger"
+var3 = "Honey Bee"
+var4 = "Honey Badger"
+print(var1.lower() + " " + var2.lower() + " " + var3.lower() + " " + var4.lower())
+print(var1.upper() + " " + var2.upper() + " " + var3.upper() + " " + var4.upper())
+
+string1 = "   Filet Mignon"
+string2 = "Brisket   "
+string3 = "   Cheeseburger   "
+print(string1)
+print(string2)
+print(string3)
+print(string1.lstrip())
+print(string2.rstrip())
+print(string3.strip())
+
+string4 = "Becomes"
+string5 = "becomes"
+string6 = "BEAR"
+string7 = "   bEautigul"
+print(string4.startswith("be"))
+print(string5.startswith("be"))
+print(string6.startswith("be"))
+print(string7.startswith("be"))
+
+print(string4.startswith("Be"))
+print(string5.startswith("be"))
+print(string6.startswith("BE"))
+print(string7.startswith("   "))
